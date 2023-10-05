84dfea917cf2ad30c82974ade14d82dfe20e736a
Andrew Burks <86085214+burksbuilds@users.noreply.github.com>
2023-10-05 15:03:40 -0400

    Switched to PreferencesCLI library
    saved a few hundred lines of code!

--- transmitter/src/transmitter/transmitter.ino
+++ transmitter/src/transmitter/transmitter.ino
@@ -5,6 +5,7 @@
 #include <nvs_flash.h>
 #include <Preferences.h>
 #include <SimpleCLI.h>
+#include <PreferencesCLI.h>
 #include "hardware.h"
 #include "common.h"
 #include "screen_icons.h"
@@ -17,6 +18,7 @@
 //globals from libraries
 Preferences preferences;
 SimpleCLI cli;
+PreferencesCLI prefCli(preferences);
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C screen(U8G2_R2);
 
 //globals for transmitter states
@@ -40,9 +42,6 @@ float batteryVoltageMultiplier;
 //globals for CLI
 char cliResponseBuffer[256];
 Command helpCommand;//command used to display CLI help
-Command setPrefCommand;//command used to set preferences
-Command getPrefCommand;//command used to set preferences
-Command clearPrefCommand;//command used to clear preferences, namespaces, and the whole storage area
 Command restartCommand;//command used to restart the transmitter or receiver
 Command getVarCommand;//used to return values of certain internal variables
 long restartTime;//if non zero, board will restart if millis() > restart
@@ -141,6 +140,11 @@ void loop() {
     String cliInput = Serial.readStringUntil('\n');
     Serial.println(cliInput);
     cli.parse(cliInput);
+    if(cli.available())
+    {
+      Command command = cli.getCommand();
+      prefCli.handleCommand(command,Serial);
+    }
     Serial.println();
     Serial.print("> ");
   }
@@ -252,33 +256,17 @@ bool cliSetup()
   cli.setErrorCallback(cliErrorCallback);
 
   helpCommand = cli.addCommand("help", helpCommandCallback);
-  helpCommand.setDescription("Use to display details on how to use a specific command\r\nAvailable Commands:\r\n\tsetPreference (setp)\r\n\tgetPreference (getp)\r\n\tclearPreference (clearp)\r\n\trestart");
+  helpCommand.setDescription("Use to display details on how to use a specific command\r\nAvailable Commands:\r\n\tsetPreference (setp)\r\n\tgetPreference (getp)\r\n\tclearPreference (clearp)\r\n\trestart\r\n\tgetVariable");
   helpCommand.addPositionalArgument("commandName,cmd","");
 
-  setPrefCommand = cli.addCommand("setP/ref/erence,setp", setPreferenceCommandCallback);
-  setPrefCommand.setDescription("Used to write the value of entries using the built-in 'Preferences' module.\r\n\tnamespace (-ns): the namespace of the preference setting\r\n\tkey (-k): the string identifier of the preference\r\n\ttype (-t): the type of the data to write\r\n\tvalue (-v): the value to store");
-  setPrefCommand.addPositionalArgument("namespace,ns");
-  setPrefCommand.addPositionalArgument("k/ey");
-  setPrefCommand.addPositionalArgument("t/ype");
-  setPrefCommand.addPositionalArgument("v/al/ue");
-
-  getPrefCommand = cli.addCommand("getP/ref/erence,getp", getPreferenceCommandCallback);
-  getPrefCommand.setDescription("Used to read the value of entries using the built-in 'Preferences' module.\r\n\tnamespace (-ns): the namespace of the preference setting\r\n\tkey (-k): the string identifier of the preference\r\n\ttype (-t): the type of the data to read");
-  getPrefCommand.addPositionalArgument("namespace,ns");
-  getPrefCommand.addPositionalArgument("k/ey");
-  getPrefCommand.addPositionalArgument("t/ype");
-
-  clearPrefCommand = cli.addCommand("clearP/ref/erence,clearp", clearPreferenceCommandCallback);
-  clearPrefCommand.setDescription("Used to clear individual preferences, entire namespaces, or the entire nvs storage area.\r\n\tnamespace (-ns): the namespace of the preference setting\r\n\tkey (-k): the string identifier of the preference");
-  clearPrefCommand.addPositionalArgument("namespace,ns","");
-  clearPrefCommand.addPositionalArgument("k/ey","");
+  prefCli.registerCommands(cli);
 
   restartCommand = cli.addCommand("restart", restartCommandCallback);
   restartCommand.setDescription("Used to restart the transmitter (-t) and/or the receiver (-r)");
   restartCommand.addFlagArgument("t/ransmitter");
   restartCommand.addFlagArgument("r/eceiver");
 
-  getVarCommand = cli.addCommand("getV/ar,getv", getVariableCommandCallback);
+  getVarCommand = cli.addCommand("getV/ar/iable,getv", getVariableCommandCallback);
   getVarCommand.setDescription("Used to get the current value of the specified variable");
   getVarCommand.addPositionalArgument("v/ar/iable");
 
@@ -313,141 +301,6 @@ void helpCommandCallback(cmd* commandPointer)
   } else {
     Serial.println(helpCommand.toString());
   }
-}
-
-void setPreferenceCommandCallback(cmd* commandPointer)
-{
-  Command cmd(commandPointer);
-  Argument name = cmd.getArg("namespace");
-  Argument key = cmd.getArg("key");
-  Argument type = cmd.getArg("type");
-  Argument value = cmd.getArg("value");
-
-  preferences.begin(name.getValue().c_str());
-
-  String typeString = type.getValue();
-  size_t bytesWritten = 0;
-  
-  if(typeString.equalsIgnoreCase("Float") )
-  {
-    bytesWritten = preferences.putFloat(key.getValue().c_str(), value.getValue().toFloat()); 
-  } else if(typeString.equalsIgnoreCase("Bytes") ) {
-    String valueText = value.getValue();
-    valueText.toUpperCase();
-    const char * valueTextPtr = valueText.c_str();
-    size_t dataLength = (valueText.length()+1)/2;
-    char * data = (char*)malloc(dataLength);
-    for(int i=0; i < dataLength; ++i)
-    {
-      char mostSig = valueTextPtr[i*2];
-      char leastSig = valueTextPtr[i*2+1];
-      data[i] = 16*((mostSig > 64)?(mostSig-55):(mostSig-48)) + ((leastSig > 64)?(leastSig-55):(leastSig-48));
-    }
-    bytesWritten = preferences.putBytes(key.getValue().c_str(), data, dataLength);
-    free(data);
-  } else {
-    sprintf(cliResponseBuffer,"ERROR: unable to store preferences of type '%s'",typeString.c_str());
-    Serial.println(cliResponseBuffer);
-  }
-
-  if (bytesWritten > 0){
-    sprintf(cliResponseBuffer,"'%s' stored as %s (%d Bytes) in %s/%s",value.getValue(),typeString,bytesWritten,name.getValue().c_str(), key.getValue().c_str());
-  } else {
-    sprintf(cliResponseBuffer,"ERROR: unable to store '%s' as %s in %s/%s",value.getValue().c_str(),typeString.c_str(),name.getValue().c_str(), key.getValue().c_str());
-  }
-  Serial.println(cliResponseBuffer);
-  preferences.end(); 
-}
-
-void getPreferenceCommandCallback(cmd* commandPointer)
-{
-  Command cmd(commandPointer);
-  Argument name = cmd.getArg("namespace");
-  Argument key = cmd.getArg("key");
-  Argument type = cmd.getArg("type");
-
-  preferences.begin(name.getValue().c_str(),true);
-
-  if(!preferences.isKey(key.getValue().c_str()))
-  {
-    sprintf(cliResponseBuffer,"ERROR: unable to locate key '%s' in namespace '%s'",key.getValue().c_str(),name.getValue().c_str());
-    Serial.println(cliResponseBuffer);
-  } else {
-
-    String typeString = type.getValue();
-    size_t bytesWritten = 0;
-    
-    if(typeString.equalsIgnoreCase("Float") )
-    {
-      Serial.println(preferences.getFloat(key.getValue().c_str()),4); 
-    } else if(typeString.equalsIgnoreCase("Bytes")){
-      size_t dataLength = preferences.getBytesLength(key.getValue().c_str());
-      char * data = (char*)malloc(dataLength);
-      char * text = (char*)malloc(dataLength*2+1);
-      preferences.getBytes(key.getValue().c_str(), data, dataLength);
-      for(int i=0; i < dataLength; ++i)
-      {
-        char mostSig = data[i] / 16;
-        char leastSig = data[i] - 16*mostSig;
-        text[i*2] = (mostSig>9)?(55+mostSig):(48+mostSig);
-        text[i*2+1] = (leastSig>9)?(55+leastSig):(48+leastSig);
-      }
-      text[dataLength*2-1] = '\0';
-      Serial.println(text);
-      free(text);
-      free(data);
-    } else {
-      sprintf(cliResponseBuffer,"ERROR: unable to store preferences of type '%s'",typeString);
-      Serial.println(cliResponseBuffer); 
-      return;
-    }
-  }
-
-  preferences.end(); 
-}
-
-void clearPreferenceCommandCallback(cmd* commandPointer)
-{
-  Command cmd(commandPointer);
-  Argument name = cmd.getArg("namespace");
-  Argument key = cmd.getArg("key");
-
-  if(key.isSet() && name.isSet())
-  {
-    sprintf(cliResponseBuffer,"'%s' has been cleared from namespace '%s'",key.getValue(),name.getValue());
-    if(!preferences.begin(name.getValue().c_str()))
-    {
-      sprintf(cliResponseBuffer,"ERROR: unable to open namespace '%s'",name.getValue());
-    } else 
-    {
-      if(preferences.isKey(key.getValue().c_str()))
-      {
-        if(!preferences.remove(key.getValue().c_str()))
-        {
-          sprintf(cliResponseBuffer,"ERROR: unable to clear '%s' from namespace '%s'",key.getValue(),name.getValue());
-        }
-      }
-      preferences.end();
-    }
-  } else if (name.isSet())
-  {
-    sprintf(cliResponseBuffer,"namespace '%s' has been cleared",name.getValue());
-    if(!preferences.begin(name.getValue().c_str()))
-    {
-      sprintf(cliResponseBuffer,"ERROR: unable to open namespace '%s'",name.getValue());
-    } else {
-      if(!preferences.clear())
-      {
-        sprintf(cliResponseBuffer,"ERROR: unable to clear namespace '%s'",name.getValue());
-      }
-      preferences.end();
-    }
-  } else {
-    nvs_flash_erase();
-    nvs_flash_init();
-    sprintf(cliResponseBuffer,"All preferences have been cleared from the NVS flash");
-  }
-  Serial.println(cliResponseBuffer);
 }
 
 void restartCommandCallback(cmd* commandPointer)
